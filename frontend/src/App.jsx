1e5ebf1a181325b530be31a4ab0548bb83d69250
Ryan Mathews <mathewsryan2@gmail.com>
2026-03-10 07:27:54 -0500

    Style: Added routing between pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,23 @@
-import { Button } from "@/components/ui/button";
 import Login from "./pages/login";
+import DashBoard from "./pages/DashBoard";
+import { BrowserRouter, Router, Route, Routes } from "react-router-dom";
+import AuthLayout from "./layouts/authlayout";
+import MainLayout from "./layouts/mainlayout";
 
 function App() {
-  return <Login />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AuthLayout />}>
+          <Route path="/" element={<Login />} />
+        </Route>
+
+        <Route element={<MainLayout />}>
+          <Route path="/dashboard" element={<DashBoard />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
