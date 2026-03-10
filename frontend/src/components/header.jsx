3f16b5e5483d23bc87af4a3e634d80531a3e433b
Ryan Mathews <mathewsryan2@gmail.com>
2026-03-10 07:26:30 -0500

    Style: Created dynamic header to change based on login or dashboard

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -1,8 +1,20 @@
-export default function header() {
+import Logo from "../assets/Logo.jpg";
+
+export default function Header({ minimal }) {
   return (
     <>
-      <img src="placeholder" alt="placehodler" />
-      <p>Test</p>
+      <header className="bg-background">
+        <img className="size-24 rounded-full" src={Logo} alt="Logo" />
+
+        {!minimal && (
+          <nav>
+            <button type="Home">Home</button>
+            <button type="Logs">Logs</button>
+            <button type="Wiki">Wiki</button>
+            <button type="Blog"></button>
+          </nav>
+        )}
+      </header>
     </>
   );
 }
