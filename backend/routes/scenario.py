0bf5fdc707f1fa66b02e2a19a3181ca2a261380a
Ryan Mathews <mathewsryan2@gmail.com>
2026-04-09 15:09:44 -0500

    Feature: Changed the logs to web sockets

--- backend/routes/scenario.py
+++ backend/routes/scenario.py
@@ -1,21 +1,30 @@
 from services.scenario import Scenario
-from fastapi import APIRouter, Depends
-from schemas import DefendRequest 
+from fastapi import APIRouter, Depends, WebSocket
+from schemas import DefendRequest
+import asyncio
+
 scenario = Scenario()  # create one instance to share across routes
 router = APIRouter(prefix="/scenario")
 
+
 @router.get("/start")
 def start_scenario():
-      ip = scenario.generate_attacker_ip()
-      return {"attacker_ip": ip}
+    ip = scenario.generate_attacker_ip()
+    return {"attacker_ip": ip}
 
-@router.get("/log")
-def start_logs():
-      return scenario.generate_logs()
+
+@router.websocket("/logs")
+async def get_logs(websocket: WebSocket):
+    await websocket.accept()
+    while True:
+        log = scenario.generate_logs()
+        await websocket.send_text(log)
+        await asyncio.sleep(2)
+
 
 @router.post("/defend")
 def end_scenario(request: DefendRequest):
-      if scenario.check_ip(request.ip):
-          return {"result": "Correct"}
-      else:
-          return {"result": "Wrong"}
+    if scenario.check_ip(request.ip):
+        return {"result": "Correct"}
+    else:
+        return {"result": "Wrong"}
