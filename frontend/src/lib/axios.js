aa639d6d74b5b494d2988bd4a253e224a50b39eb
Ryan Mathews <mathewsryan2@gmail.com>
2026-04-11 15:36:08 -0500

    Bug: Signup bug *fix

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://special-space-engine-7xrgxw6wv4ghpj7p-8000.app.github.dev",
-  headers: { "Content-Type": "application/json"},
+  baseURL: "http://localhost:8000",
 });
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = 'Bearer ${token}';
+  if (token) config.headers.Authorization = "Bearer ${token}";
   return config;
 });
 
 export default api;
+
