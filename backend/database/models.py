1ce8c4478aec970fb1ae9ce515835135dfafd539
Ryan Mathews <mathewsryan2@gmail.com>
2026-04-12 23:54:45 -0500

    Clean: removed unused model

--- backend/database/models.py
+++ backend/database/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String
 from database.db import Base
 
 
-# Created user model to follow for inserting into the database 
+# Created user model to follow for inserting into the database
 class User(Base):
     __tablename__ = "users"
 
@@ -12,10 +12,10 @@ class User(Base):
 
 
 # Created commands model to follow for recieving from the database
-class Commands(Base):
-    __tablename__ = "commands"
-
-    id = Column(Integer, primary_key=True, index=True)
-    command = Column(String, unique=True, nullable=False)
-    description = Column(String, nullable=False)
-    output = Column(String, nullable=False)
+# class Commands(Base):
+#     __tablename__ = "commands"
+#
+#     id = Column(Integer, primary_key=True, index=True)
+#     command = Column(String, unique=True, nullable=False)
+#     description = Column(String, nullable=False)
+#     output = Column(String, nullable=False)
